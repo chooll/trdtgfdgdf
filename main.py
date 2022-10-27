1a5d5a0cd50d29adcff71c0edb510ea72bcc13a4
ANAT <good.anat@yandex.ru>
2022-10-27 19:38:36 +0300

    Переделал что-то

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ file = open("numbers.txt", "r")
 
 for string in file:
     t = string.replace("\n", "")
-    print ( f"Квадрат числа {t} равен {float(t)**2}")
 
+t = 1
+n = 5
+replace();f dfdf
 
 
 
